ead8d6e7cad39338ff99fbd10f6543528d88dbac
mugi95 <archmichaelmugi95@gmail.com>
2023-02-21 13:43:04 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,16 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int n;
 
-    n = add(89, 9);
-    printf("%d\n", n);
-    return (0);
-}
+	/**
+	 * main - check the code
+	 *
+	 * Return: Always 0.
+	 */
+	int main(void)
+	{
+	    print_to_98(0);
+	    print_to_98(98);
+	    print_to_98(111);
+	    print_to_98(81);
+	    print_to_98(-10);
+	    return (0);
+	}
+
